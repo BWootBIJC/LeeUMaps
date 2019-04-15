43b7224dc5a016383ffc5edca9507dac1fb3e5ff
swift-mw <swift-mw@hsb446-01.jeffersonstate.edu>
2019-04-15 11:46:30 -0500

    Added existing project to new repo

--- mapsForLeeU/ViewController.swift
+++ mapsForLeeU/ViewController.swift
@@ -7,14 +7,21 @@
 //
 
 import UIKit
+import MapKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UISearchBarDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-
+    @IBOutlet weak var map: MKMapView!
+    @IBAction func search(_ sender: Any) {
+        let searchController = UISearchController(searchResultsController: nil)
+        searchController.searchBar.delegate = self
+    }
+    
+    
+    
 }
 
